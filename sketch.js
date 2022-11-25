8636d25f17625037e0a2e61f89d4c26e33ba9cd2
moondae1 <119025618+moondae1@users.noreply.github.com>
2022-11-25 14:16:43 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,44 +8,69 @@
 
 
 let song;
+let button;
 function setup()
 {
+    createCanvas(640, 480);
     soundFormats('mp3','ogg');
-    song = loadSound('music',loaadMusic);
+    song = loadSound('music',loadMusic);
     // musicFile.play();
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
 }
 
-
-function loadMusic()
-{
-    song.play();
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        button.html('play');
+    }else{
+        song.stop();
+        button.html('play');
+    }
 }
 
+function loaded(){
+    console.log("It's working");
+}
 
 function draw(){
     fill(255,0.0);
     ellipse(50,50,100,100);
 }
 
-var song;
-var button;
 
-function setup(){
-    createCanvas(200, 200);
-    song = loadSound('music.mp3', loaded);
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-    background(51);
-}
 
-function togglePlaying(){
-    if(!song.isPlaying()){
-        song.play();
-        song.setVolume(0.3);
-        button.html('play');
-    }
-}
 
-function loaded(){
-    console.log('loaded');
-}
+// function loadMusic()
+// {
+//     song.play();
+// }
+
+
+// function draw(){
+//     fill(255,0.0);
+//     ellipse(50,50,100,100);
+// }
+
+// var song;
+// var button;
+
+// function setup(){
+//     createCanvas(200, 200);
+//     song = loadSound('music.mp3', loaded);
+//     button = createButton('play');
+//     button.mousePressed(togglePlaying);
+//     background(51);
+// }
+
+// function togglePlaying(){
+//     if(!song.isPlaying()){
+//         song.play();
+//         song.setVolume(0.3);
+//         button.html('play');
+//     }
+// }
+
+// function loaded(){
+//     console.log('loaded');
+// }
