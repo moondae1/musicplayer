0abf38795ea00ed575362b24ff05f861151f41cd
moondae1 <119025618+moondae1@users.noreply.github.com>
2022-11-25 13:10:29 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+// function setup()
+// {
+//     createCanvas(400, 400);
+//     line(-50, 120, 40, -60);
+// line(40, -60, 10, 200);
+
+// }
+
+
+let mySound;
+function setup()
+{
+    soundFormats('mp3','ogg');
+    mySound = loadSound('music',loaadMusic);
+
+
+}
+
+
+function setup()
+
